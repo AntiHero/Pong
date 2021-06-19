f4a7d8ed99786fa4098f806853711a2225f84a07
Aleksandr Schemelev <prophreaker@gmail.com>
2021-06-19 23:53:58 +0300

    feat: add paddle and bounds interception logic

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ ball_color = 54, 185, 255
 paddle = pg.Rect(350, 580, 100, 20)
 paddle_color = 255, 255, 255
 
-directions = {pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0)}
+step = 10
+directions = {pg.K_LEFT: (-step, 0), pg.K_RIGHT: (step, 0)}
 
 while True:
     for event in pg.event.get():
@@ -33,6 +34,12 @@ while True:
     if event.type == pg.KEYDOWN:
         if event.key in directions:
             v = directions[event.key]
+
+            if paddle.left < 0:
+                paddle.left = 0
+            elif paddle.right > SCREEN_WIDTH:
+                paddle.right = SCREEN_WIDTH
+
             paddle.move_ip(v)
 
     pg.display.update()

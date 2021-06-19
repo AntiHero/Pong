0c3808c8c8565cc5b6fdf6ebd57ece9690ec38e9
Aleksandr Schemelev <prophreaker@gmail.com>
2021-06-19 23:25:06 +0300

    feat: move paddle on left/right key press

--- main.py
+++ main.py
@@ -17,12 +17,22 @@ ball_color = 54, 185, 255
 paddle = pg.Rect(350, 580, 100, 20)
 paddle_color = 255, 255, 255
 
+directions = {pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0)}
+
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
-
+   
+    screen.fill((0, 0, 0))
+    
     pg.draw.rect(screen, ball_color, ball)
     pg.draw.rect(screen, paddle_color, paddle)
+
+    if event.type == pg.KEYDOWN:
+        if event.key in directions:
+            v = directions[event.key]
+            paddle.move_ip(v)
     
     pg.display.update()
     clock.tick(TICK_RATE)
